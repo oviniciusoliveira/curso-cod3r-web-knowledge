e149f913048108cfc3c2bb4e98dd6fb6e579ef35
Vinícius de Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2020-12-29 18:33:56 -0300

    Implementação de middlewares com o cors; Configuracao do postgres e criacao de migrations com o knex

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
+const consign = require('consign')
+const db = require('./config/db')
+
+app.db = db
+
+consign()   
+    .then('./api')
+    .then('./config/middlewares.js')
+    .then('./config/routes.js')
+    .into(app)
 
 app.listen(3000, () => {
     console.log('Backend executando...')
