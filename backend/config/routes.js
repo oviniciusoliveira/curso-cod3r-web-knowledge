29909259912145833b81be17ddf67e72876ca345
Vinícius de Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2021-01-01 21:21:03 -0300

    Criado Api de artigos e implementado sistema de paginacao

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -11,10 +11,18 @@ module.exports = (app) => {
   // Necessário que /categories/tree venha antes de :id
 
   app.route("/categories/tree").get(app.api.category.getTree);
-  
+
   app
     .route("/categories/:id")
     .get(app.api.category.getById)
     .put(app.api.category.save)
     .delete(app.api.category.remove);
+
+  app.route("/articles").get(app.api.article.get).post(app.api.article.save);
+
+  app
+    .route("/articles/:id")
+    .get(app.api.article.getById)
+    .put(app.api.article.save)
+    .delete(app.api.article.remove);
 };
