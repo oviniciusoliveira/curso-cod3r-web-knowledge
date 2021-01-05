95dde19d962e3fb6fad43ddd3e387a26f40df06c
Vinícius de Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2021-01-05 13:37:49 -0300

    Api de estatísticas com MongoDB utilizando mongoose

---
+++ backend/config/mongodb.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const uri = "mongodb://localhost/knowledge_stats";
+// mongoose.set("useUnifiedTopology", true);
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    // useUnifiedTopolog    y: true,
+  })
+  .catch((e) => {
+    /* Node Console.Log Callers */
+    const msg = "ERRO: Nao foi possivel conectar com o MongoDB!";
+    console.log("\x1b[41m%s\x1b[37m", msg, "\x1b[0m");
+  });
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const consign = require("consign");
 const db = require("./config/db");
+const mongoose = require("mongoose");
+
+require("./config/mongodb");
 
 app.db = db;
+app.mongoose = mongoose;
 
 consign()
-  .include('./config/passport.js')
+  .include("./config/passport.js")
   .then("./api/validator.js")
   .then("./api")
   .then("./config/middlewares.js")
