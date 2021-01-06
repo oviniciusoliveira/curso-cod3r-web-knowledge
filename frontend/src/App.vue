3e56e200ea123d3b5e9c07cbbfc3831a50f0858c
Vinícius de Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2021-01-06 18:45:44 -0300

    Alternancia da visiblidade do menu com botao toggle utilizando vuex

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app">
+	<div id="app" :class="{'hide-menu' : !isMenuVisible}">
 		<Header title="Knowledge" :hideToggle="false"/>
 		<Menu />
 		<Content />
@@ -8,36 +8,45 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import Header from "./components/template/Header";
 import Menu from "./components/template/Menu";
 import Content from "./components/template/Content";
 import Footer from "./components/template/Footer";
 export default {
   name: "App",
-  components: { Header, Menu, Content, Footer }
+  components: { Header, Menu, Content, Footer },
+  computed: mapState(["isMenuVisible"]),
 };
 </script>
 
 <style>
-	* {
-		font-family: 'Lato', sans-serif;
-	}
+* {
+  font-family: "Lato", sans-serif;
+}
 
-	body {
-		margin: 0;
-	}
+body {
+  margin: 0;
+}
 
-	#app {
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-		height: 100vh;
-		display: grid;
-		grid-template-rows: 60px 1fr 40px;
-		grid-template-columns: 300px 1fr;
-		grid-template-areas: 
-			"header header"
-			"menu content"
-			"menu footer";
-	}
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 60px 1fr 40px;
+  grid-template-columns: 300px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "menu footer";
+}
+
+#app.hide-menu {
+  grid-template-areas:
+    "header header"
+    "content content"
+    "footer footer";
+}
 </style>
--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -18,14 +18,16 @@ export default {
   },
   computed: {
     icon() {
-      return "fa-angle-left"
-    }
+      return this.$store.state.isMenuVisible
+        ? "fa-angle-left"
+        : "fa-angle-down";
+    },
   },
   methods: {
     toggleMenu() {
-      console.log('toggle menu')
-    }
-  }
+      this.$store.commit("toggleMenu");
+    },
+  },
 };
 </script>
 <style>
