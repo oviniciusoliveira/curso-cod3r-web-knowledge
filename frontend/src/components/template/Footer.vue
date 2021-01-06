65b541819c45188e10b06336bd993359869852ff
Vinícius de Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2021-01-06 17:51:43 -0300

    Estrutura inicial do template

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="footer">
+        <span>Desenvolvido por <strong>Vinícius Oliveira</strong> © 2021</span>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+.footer {
+  grid-area: footer;
+  background-color: #dddddd;
+  color: #333333;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 10px;
+}
+</style>
