a9df9152847b8fcb28766c9634bd58d41c67d49a
Vinícius de Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2021-01-07 17:20:59 -0300

    Configuracao do vue-toasted com default messages

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -1,1 +1,16 @@
-export const baseApiUrl = 'http://localhost:3000'
+import Vue from "vue";
+
+export const baseApiUrl = "http://localhost:3000";
+
+export function showError(error) {
+  if (error && error.response && error.response.data) {
+    Vue.toasted.global.defaultError({ message: error.response.data });
+  } else if (typeof error === "string") {
+    Vue.toasted.global.defaultError({ message: error });
+  } else {
+    Vue.toasted.global.defaultError();
+  }
+}
+
+// Alternativa 2: 
+// export default { baseApiUrl, showError }
