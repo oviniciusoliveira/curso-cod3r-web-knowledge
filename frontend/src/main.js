72c219c9d809d66e1ae6fe45265bb6e8bf92665b
Vinícius de Oliveira <vinicius.deoliveira@aluno.ufabc.edu.br>
2021-01-07 15:25:12 -0300

    Adicionado componente Stat em Home; Conexao com a Api do mongo atraves do axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,21 @@
-import 'font-awesome/css/font-awesome.css'
-import Vue from 'vue'
+import "font-awesome/css/font-awesome.css";
+import Vue from "vue";
 
-import App from './App'
+import App from "./App";
 
-import './config/bootstrap'
-import store from './config/store'
-import router from './config/router'
+import "./config/bootstrap";
+import store from "./config/store";
+import router from "./config/router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// TEMPORARIO!
+const token =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibm9tZSI6IlZpbmljaXVzIE9saXZlaXJhIiwiZW1haWwiOiJ2aW5pY2l1c0Brbm93bGVkZ2UuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYxMDA0MjcxNSwiZXhwIjoxNjEwMTI5MTE1fQ.SOXCNfceXvkIq5cG4SN4hnbKp_DcTFdiAJT486Yopgs";
+require("axios").defaults.headers.common["Authorization"] = `bearer ${token}`;
 
 new Vue({
   store,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
